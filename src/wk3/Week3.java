8bd27b1ce7557fc1df42bfbcc74ae06acadf1e51
Ben <ben.blanc@georgebrown.ca>
2024-09-19 12:52:15 -0400

    Class Code Sept 19, 2024 @ 12h50

---
+++ src/wk3/Week3.java
@@ -0,0 +1,19 @@
+package wk3;
+
+public class Week3 {
+
+    //psvm+tab
+    public static void main(String[] args) {
+
+    }
+    static void additionStorm(){
+        /*
+                Continually ask user to answer
+                an addition equation
+                until the user correctly answers
+                5 questions
+         */
+
+    }
+
+}
